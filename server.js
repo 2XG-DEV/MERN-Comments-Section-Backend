bc668d46439a686611812fd0ca09f80567fb54fd
radu <radutarean@gmail.com>
2022-02-17 20:46:35 +0200

    backend change

--- server.js
+++ server.js
@@ -4,6 +4,22 @@ dotenv.config();
 import connectDB from "./config/db.js";
 connectDB();
 
+var allowCrossDomain = function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization, Content-Length, X-Requested-With"
+  );
+
+  // intercept OPTIONS method
+  if ("OPTIONS" == req.method) {
+    res.send(200);
+  } else {
+    next();
+  }
+};
+
 import express from "express";
 
 import commentRoutes from "./routes/commentRoutes.js";
@@ -13,6 +29,8 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
+
+app.use(allowCrossDomain);
 
 app.listen(
   PORT,
